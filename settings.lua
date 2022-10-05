08736ada06bfc56b0cac10641b83509a22d69b52
Bronuh <bronyx@yandex.ru>
2022-10-05 21:21:58 +0300

    Кака

--- settings.lua
+++ settings.lua
@@ -1,32 +1,10 @@
 _G.data:extend({
 	{
-		type = "double-setting",
-		name = "abro-tweaks-maxfactor",
-		setting_type = "runtime-global",
+		type = "int-setting",
+		name = "infinity-factory-additional-tiers",
+		setting_type = "startup",
         minimum_value = 0,
-		maximum_value = 1,
+		maximum_value = 10000,
 		default_value = 1
-	},
-	{
-		type = "bool-setting",
-		name = "abro-tweaks-global-chat",
-		setting_type = "runtime-global",
-		default_value = true
-	},
-	{
-		type = "double-setting",
-		name = "abro-tweaks-chat-color-multiplier",
-		setting_type = "runtime-global",
-        minimum_value = 0,
-		maximum_value = 1,
-		default_value = 0.8
-	},
-	{
-		type = "double-setting",
-		name = "abro-tweaks-teleport-radius",
-		setting_type = "runtime-global",
-        minimum_value = 0,
-		maximum_value = 32,
-		default_value = 5
 	}
 })
