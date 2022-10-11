0b779a62ca39e02499b82cccf4dff2d47e535068
KeyJ148 <kejik148@gmail.com>
2022-10-11 17:25:45 +0600

    Add settings to config

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.additional_levels = settings.startup["infinity-factory-additional-levels"].value
+Config.upgrade_multiplier = settings.startup["infinity-factory-upgrade-multiplier"].value
+Config.cost_multiplier = settings.startup["infinity-factory-cost-multiplier"].value
--- settings.lua
+++ settings.lua
@@ -1,10 +1,26 @@
 _G.data:extend({
 	{
 		type = "int-setting",
-		name = "infinity-factory-additional-tiers",
+		name = "infinity-factory-additional-levels",
 		setting_type = "startup",
         minimum_value = 0,
-		maximum_value = 10000,
-		default_value = 10
+		maximum_value = 10,
+		default_value = 4
+	},
+	{
+		type = "int-setting", -- TODO double?
+		name = "infinity-factory-upgrade-multiplier",
+		setting_type = "startup",
+        minimum_value = 2,
+		maximum_value = 16,
+		default_value = 2
+	},
+	{
+		type = "int-setting", -- TODO double?
+		name = "infinity-factory-cost-multiplier",
+		setting_type = "startup",
+        minimum_value = 2,
+		maximum_value = 16,
+		default_value = 4
 	}
 })
