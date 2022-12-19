1c79d2a959574d58d12b30f0f25584d6bec71876
KeyJ148 <kejik148@gmail.com>
2022-12-19 13:16:36 +0300

    Add assembler and boiler

--- prototypes/entity/entities/assembler.lua
+++ prototypes/entity/entities/assembler.lua
@@ -4,6 +4,13 @@ local PREFIX_NAME = "assembling-machine-"
 
 local function object_updater(zero_object, level, object)
     object.crafting_speed = zero_object.crafting_speed * math.pow(Config.upgrade_multiplier, level)
+    for i, fluid_box in ipairs(zero_object.fluid_boxes) do
+        local zero_object_fluid_box_height = 1
+        if (fluid_box.height ~= nil) then
+            zero_object_fluid_box_height = fluid_box.height
+        end
+        object.fluid_boxes[i].height = zero_object_fluid_box_height * math.pow(Config.upgrade_multiplier, level)
+    end
 end
 
--- TODO add_data_all_levels(EXAMPLE_TYPE, EXAMPLE_NAME, PREFIX_NAME, object_updater, Consts.EMPTY_FUNCTION, Consts.EMPTY_FUNCTION)
+add_data_all_levels(EXAMPLE_TYPE, EXAMPLE_NAME, PREFIX_NAME, object_updater, Consts.EMPTY_FUNCTION, Consts.EMPTY_FUNCTION)
---
+++ prototypes/entity/entities/boiler.lua
@@ -0,0 +1,21 @@
+local EXAMPLE_TYPE = "boiler"
+local EXAMPLE_NAME = "boiler"
+local PREFIX_NAME = "boiler-"
+
+local function object_updater(zero_object, level, object)
+    object.energy_consumption = change_energy_pow_upgrade(zero_object.energy_consumption, level)
+
+    local zero_object_height = 1
+    if (zero_object.fluid_box.height ~= nil) then
+        zero_object_height = zero_object.fluid_box.height
+    end
+    object.fluid_box.height = zero_object_height * math.pow(Config.upgrade_multiplier, level)
+
+    local zero_object_output_height = 1
+    if (zero_object.output_fluid_box.height ~= nil) then
+        zero_object_output_height = zero_object.output_fluid_box.height
+    end
+    object.output_fluid_box.height = zero_object_output_height * math.pow(Config.upgrade_multiplier, level)
+end
+
+add_data_all_levels(EXAMPLE_TYPE, EXAMPLE_NAME, PREFIX_NAME, object_updater, Consts.EMPTY_FUNCTION, Consts.EMPTY_FUNCTION)
