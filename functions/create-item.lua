2b763c4c00556f4821b0378835861baf9f172fd7
KeyJ148 <kejik148@gmail.com>
2022-12-22 13:18:06 +0300

    Refactoring create-item script

--- functions/create-item.lua
+++ functions/create-item.lua
@@ -1,16 +1,15 @@
-function create_item_with_level(exmaple_name, prefix_name, level)
-    return create_item_with_level(exmaple_name, prefix_name, level, Consts.EMPTY_FUNCTION)
+function create_item_with_level(exmaple_type, exmaple_name, prefix_name, level)
+    return create_item_with_level(exmaple_type, exmaple_name, prefix_name, level, Consts.EMPTY_FUNCTION)
 end
 
-function create_item_with_level(exmaple_name, prefix_name, level, item_updater)
+function create_item_with_level(exmaple_type, exmaple_name, prefix_name, level, item_updater)
     validate_level(level)
     local new_name = get_name_by_level(prefix_name, level)
-    local zero_item = data.raw["item"][exmaple_name]
+    local zero_item = data.raw[exmaple_type][exmaple_name]
 
     local item = util.table.deepcopy(zero_item)
     item.name = new_name
     item.order = zero_item.order --TODO настроить порядок, чтобы в списке рецептов Т10 шёл после Т9, а не после Т1. Буква всегда одна, но может быть 2 квадратные скобки
-    item.place_result = new_name
 
     item_updater(zero_item, level, item)
     return item
