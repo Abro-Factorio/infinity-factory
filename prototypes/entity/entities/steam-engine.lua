9d74fd7d585e9e92ae186609305a24d83bccc26d
KeyJ148 <kejik148@gmail.com>
2022-12-25 13:19:22 +0300

    Add steam-engine

---
+++ prototypes/entity/entities/steam-engine.lua
@@ -0,0 +1,15 @@
+local EXAMPLE_TYPE = "generator"
+local EXAMPLE_NAME = "steam-engine"
+local PREFIX_NAME = "steam-engine-"
+
+local function object_updater(zero_object, level, object)
+    object.fluid_usage_per_tick = zero_object.fluid_usage_per_tick * math.pow(Config.upgrade_multiplier, level)
+
+    local zero_object_height = 1
+    if (zero_object.fluid_box.height ~= nil) then
+        zero_object_height = zero_object.fluid_box.height
+    end
+    object.fluid_box.height = zero_object_height * math.pow(Config.upgrade_multiplier, level)
+end
+
+add_data_all_levels(EXAMPLE_TYPE, EXAMPLE_NAME, PREFIX_NAME, object_updater, Consts.EMPTY_FUNCTION, Consts.EMPTY_FUNCTION)
