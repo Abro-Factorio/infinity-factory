5c976a428f0a20b72807ec14ba840b5020af00a2
KeyJ148 <kejik148@gmail.com>
2022-12-29 16:15:40 +0300

    Fix chemical-plant prototype

--- prototypes/entity/entities/chemical-plant.lua
+++ prototypes/entity/entities/chemical-plant.lua
@@ -4,7 +4,13 @@ local PREFIX_NAME = "chemical-plant-"
 
 local function object_updater(zero_object, level, object)
     object.crafting_speed = zero_object.crafting_speed * math.pow(Config.upgrade_multiplier, level)
-    -- TODO fluid_boxes.height to output ?
+    for i, fluid_box in ipairs(zero_object.fluid_boxes) do
+        local zero_object_fluid_box_height = 1
+        if (fluid_box.height ~= nil) then
+            zero_object_fluid_box_height = fluid_box.height
+        end
+        object.fluid_boxes[i].height = zero_object_fluid_box_height * math.pow(Config.upgrade_multiplier, level)
+    end
 end
 
 add_data_all_levels(EXAMPLE_TYPE, EXAMPLE_NAME, PREFIX_NAME, object_updater, Consts.EMPTY_FUNCTION, Consts.EMPTY_FUNCTION)
